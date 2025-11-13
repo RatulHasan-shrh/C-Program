e55585b46046eae8cc221c43c3a489c9e1b53ef9
RatulHasan-shrh <ratulhasan2603@gmail.com>
2025-11-13 21:21:07 +0600

    Reformat code in character.c for consistency,,,

--- int,float,double ,character.c
+++ int,float,double ,character.c
@@ -1,10 +1,12 @@
-
 #include <stdio.h>
 
 int main() {
-    printf("Size of int: %lu bytes\n", sizeof(int));
-    printf("Size of float: %lu bytes\n", sizeof(float));
-    printf("Size of double: %lu bytes\n", sizeof(double));
-    printf("Size of char: %lu byte\n", sizeof(char));
-    return 0;
+    
+       printf("Size of int: %lu bytes\n", sizeof(int));
+       printf("Size of float: %lu bytes\n", sizeof(float));
+       printf("Size of double: %lu bytes\n", sizeof(double));
+       printf("Size of char: %lu byte\n", sizeof(char));
+    
+ return 0;
 }
+
