7efc5820377f5f70004f087b5126b582f6510d72
RatulHasan-shrh <ratulhasan2603@gmail.com>
2025-11-14 02:17:40 +0600

    Add pyramid pattern printing program
    Implement a program to print a right-aligned pyramid pattern based on user input for the number of rows.

--- 180 rotation of simple pyramid.c
+++ 180 rotation of simple pyramid.c
@@ -1,1 +1,23 @@
 
+#include <stdio.h>
+
+int main() {
+    int n, i, j, space;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++) {
+        // Print leading spaces
+        for(space = 1; space <= n - i; space++) {
+            printf(" ");
+        }
+        // Print stars
+        for(j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
