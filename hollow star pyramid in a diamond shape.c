a2190693f4274087178587b408fe97277afc7066
RatulHasan-shrh <ratulhasan2603@gmail.com>
2025-11-14 12:39:53 +0600

    hollow star pyramid in diamond shape program
    This program prints a hollow star pyramid in a diamond shape based on user input for the number of rows.

--- hollow star pyramid in a diamond shape.c
+++ hollow star pyramid in a diamond shape.c
@@ -1,1 +1,60 @@
 
+#include <stdio.h>
+
+int main() {
+    int n, i, j;
+    
+    
+
+    printf("Enter number of rows (half diamond): ");
+    scanf("%d", &n);
+    
+
+    // Upper hollow pyramid
+    for(i = 1; i <= n; i++) {
+            
+
+        // spaces
+        for(j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+        
+
+        // stars with hollow logic
+        for(j = 1; j <= 2*i - 1; j++) {
+            if (j == 1 || j == 2*i - 1)
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+        
+    }
+
+    // Lower inverted hollow pyramid
+    for(i = n - 1; i >= 1; i--) {
+
+        // spaces
+        for(j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+        
+
+        // stars with hollow logic
+        for(j = 1; j <= 2*i - 1; j++) {
+            if (j == 1 || j == 2*i - 1)
+                printf("*");
+            else
+                printf(" ");
+        }
+        
+        printf("\n");
+        
+    }
+    
+
+    return 0;
+    
+}
+
+
