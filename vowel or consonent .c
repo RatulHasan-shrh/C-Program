a9b38944792298d746201e39da125e7c039c886f
RatulHasan-shrh <ratulhasan2603@gmail.com>
2025-11-19 15:16:31 +0600

    vowel or consonent .c

--- vowel or consonent .c
+++ vowel or consonent .c
@@ -12,14 +12,16 @@ int main() {
    
     if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
 
-        // Check for vowels
+       
         if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' ||
             ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
             printf("%c is a vowel.\n", ch);
+            
 
         } else {
             printf("%c is a consonant.\n", ch);
         }
+        
 
     } else {
         printf("%c is not an alphabet character.\n", ch);
@@ -27,4 +29,5 @@ int main() {
 
     return 0;
 }
+
 
