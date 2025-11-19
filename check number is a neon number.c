9c23a4fbce039b74488cb9ac1811429122d5aff9
RatulHasan-shrh <ratulhasan2603@gmail.com>
2025-11-19 14:50:10 +0600

    check number is a neon number.c

--- check number is a neon number.c
+++ check number is a neon number.c
@@ -1,1 +1,26 @@
 
+
+#include <stdio.h>
+
+int main() {
+    int num, square, sum = 0, temp;
+    
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    
+ 
+    square = num * num;
+    temp = square;
+    
+    while(temp != 0) {
+        sum += temp % 10;
+        temp /= 10;
+    }
+    
+    if(sum == num)
+        printf("%d is a Neon number.\n", num);
+    else
+        printf("%d is not a Neon number.\n", num);
+    
+    return 0;
+}
