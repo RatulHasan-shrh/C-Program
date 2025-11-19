2277e87455ae56a277182aca03374606a36371cd
RatulHasan-shrh <ratulhasan2603@gmail.com>
2025-11-19 15:03:07 +0600

    full diamond shape pyramid program
    This program prints a full diamond shape pyramid based on user input for the number of rows in the upper half.

--- full diamond shape pyramid.c
+++ full diamond shape pyramid.c
@@ -1,1 +1,38 @@
 
+
+#include <stdio.h>
+
+int main() {
+    int n, i, j, k;
+
+    printf("Enter the number of rows for the upper half: ");
+    scanf("%d", &n);
+
+   
+    for(i = 1; i <= n; i++) {
+       
+        for(j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+       
+        for(k = 1; k <= 2*i - 1; k++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+  
+    for(i = n-1; i >= 1; i--) {
+     
+        for(j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+    
+        for(k = 1; k <= 2*i - 1; k++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
