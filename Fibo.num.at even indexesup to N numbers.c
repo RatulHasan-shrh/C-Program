25cd4ddf1bb1af758c85745535e12a9b8fe9bf40
RatulHasan-shrh <ratulhasan2603@gmail.com>
2025-11-19 14:32:29 +0600

    Fibonacci sequence for even indexes
    This program calculates and prints Fibonacci numbers at even indexes up to N.

--- Fibo.num.at even indexesup to N numbers.c
+++ Fibo.num.at even indexesup to N numbers.c
@@ -1,1 +1,33 @@
 
+
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    long long f1 = 0, f2 = 1, f3;
+  
+
+    printf("Enter N: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci numbers at even indexes:\n");
+
+   
+    for(i = 0; i < n; i++) {
+        if(i == 0) {
+            f3 = 0;
+        } else if(i == 1) {
+            f3 = 1;
+        } else {
+            f3 = f1 + f2;
+            f1 = f2;
+            f2 = f3;
+        }
+
+        if(i % 2 == 0) {   // even index
+            printf("%lld ", f3);
+        }
+    }
+
+    return 0;
+}
