5f39d6ae657afc9c61b8e7d360e93a045a547308
RatulHasan-shrh <ratulhasan2603@gmail.com>
2025-11-19 14:37:43 +0600

    Implement factor display program for natural numbers
    This program prompts the user for a natural number and displays its factors by iterating through all integers up to the number.

--- all factors of a natural numbers.c
+++ all factors of a natural numbers.c
@@ -1,1 +1,22 @@
 
+
+#include <stdio.h>
+
+int main() {
+    int num, i;
+
+    printf("Enter a natural number: ");
+    scanf("%d", &num);
+
+    printf("Factors of %d are: ", num);
+  
+
+    for(i = 1; i <= num; i++) {
+        if(num % i == 0) {   // if i divides num
+            printf("%d ", i);
+        }
+    }
+
+    return 0;
+  
+}
