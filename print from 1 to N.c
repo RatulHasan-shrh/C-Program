024959634c29215fc6a3087dfcb2b22de38a5b0f
RatulHasan-shrh <ratulhasan2603@gmail.com>
2025-11-19 15:12:42 +0600

    print from 1 to N.c

--- print from 1 to N.c
+++ print from 1 to N.c
@@ -1,1 +1,20 @@
 
+#include <stdio.h>
+
+int main() {
+    int N, i;
+
+    printf("Enter a number N: ");
+    scanf("%d", &N);
+  
+
+    printf("Numbers from 1 to %d:\n", N);
+    for(i = 1; i <= N; i++) {
+        printf("%d ", i);
+    }
+  
+    printf("\n");
+  
+
+    return 0;
+}
