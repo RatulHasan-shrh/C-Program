bf13c4fbe0f8b561c559f59c6a47a03ae020fd06
RatulHasan-shrh <ratulhasan2603@gmail.com>
2025-11-19 14:55:31 +0600

    factorial calculation in C

--- factorial number.c
+++ factorial number.c
@@ -1,1 +1,27 @@
 
+
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    unsigned long long factorial = 1;
+  
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+  
+
+    if (n < 0)
+        printf("Factorial does not exist for negative numbers.\n");
+    else {
+        for (i = 1; i <= n; i++) {
+            factorial *= i;
+        }
+      
+        printf("Factorial of %d is %llu\n", n, factorial);
+    }
+  
+
+    return 0;
+  
+}
