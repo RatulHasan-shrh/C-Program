25b25d83049afb96d4bd450db9901840ad7db5c1
RatulHasan-shrh <ratulhasan2603@gmail.com>
2025-11-19 14:40:20 +0600

    Update alphabets print by using loop.c
    Added code to print lowercase letters from 'a' to 'z'.

--- alphabets print by using loop.c
+++ alphabets print by using loop.c
@@ -1,1 +1,29 @@
 
+
+#include <stdio.h>
+
+int main() {
+    char ch;
+
+    for (ch = 'A'; ch <= 'Z'; ch++) {
+        printf("%c ", ch);
+    }
+
+    return 0;
+}
+
+
+✅ Print a to z using a loop
+c
+Copy code
+#include <stdio.h>
+
+int main() {
+    char ch;
+
+    for (ch = 'a'; ch <= 'z'; ch++) {
+        printf("%c ", ch);
+    }
+
+    return 0;
+}
