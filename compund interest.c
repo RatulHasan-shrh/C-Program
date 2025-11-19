58533f43b36d5ba5ba00c1cca7c944908a7028f6
RatulHasan-shrh <ratulhasan2603@gmail.com>
2025-11-19 14:51:13 +0600

    compound interest calculation in C

--- compund interest.c
+++ compund interest.c
@@ -1,1 +1,28 @@
 
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double principal, rate, time, compoundInterest;
+    
+   
+    printf("Enter the principal amount: ");
+    scanf("%lf", &principal);
+    
+    printf("Enter the annual interest rate (in %%): ");
+    scanf("%lf", &rate);
+    
+    printf("Enter the time (in years): ");
+    scanf("%lf", &time);
+    
+   
+    compoundInterest = principal * pow((1 + rate / 100), time);
+  
+    
+    printf("The amount after %.2lf years is: %.2lf\n", time, compoundInterest);
+    printf("The compound interest is: %.2lf\n", compoundInterest - principal);
+  
+    
+    return 0;
+}
