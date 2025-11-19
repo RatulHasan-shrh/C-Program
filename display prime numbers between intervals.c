070ac8a7700e3baec33359badb5ce6d7f0bf6a3d
RatulHasan-shrh <ratulhasan2603@gmail.com>
2025-11-19 14:54:14 +0600

    prime number display between intervals
    Implement a program to display prime numbers within a specified range.

--- display prime numbers between intervals.c
+++ display prime numbers between intervals.c
@@ -1,1 +1,38 @@
 
+
+#include <stdio.h>
+
+int main() {
+    int lower, upper, i, j, isPrime;
+  
+
+    printf("Enter lower and upper limits: ");
+    scanf("%d %d", &lower, &upper);
+  
+
+    printf("Prime numbers between %d and %d are:\n", lower, upper);
+  
+
+    for (i = lower; i <= upper; i++) {
+        if (i <= 1) 
+            continue;
+      
+
+        isPrime = 1; 
+
+        for (j = 2; j * j <= i; j++) {
+            if (i % j == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+      
+
+        if (isPrime)
+            printf("%d ", i);
+    }
+  
+
+    return 0;
+  
+}
