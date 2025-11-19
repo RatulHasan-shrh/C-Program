0647ac90a86f26c9bbdfe5d96b37a6dfcc00b999
RatulHasan-shrh <ratulhasan2603@gmail.com>
2025-11-19 14:35:56 +0600

    palindrome number checking functionality
    Implement palindrome check for a number.

--- a number is palindrome or not.c
+++ a number is palindrome or not.c
@@ -1,1 +1,29 @@
 
+
+#include <stdio.h>
+
+int main() {
+    int num, original, reversed = 0, digit;
+  
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    original = num;  // store original number
+  
+
+    while(num > 0) {
+        digit = num % 10;             // get last digit
+        reversed = reversed * 10 + digit; // form reversed number
+        num = num / 10;               // remove last digit
+    }
+  
+
+    if(original == reversed)
+        printf("%d is a palindrome number.\n", original);
+    else
+        printf("%d is NOT a palindrome number.\n", original);
+  
+
+    return 0;
+}
